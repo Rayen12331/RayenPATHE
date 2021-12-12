4c4fc926595a6c7285fe6da69f9a120b24fbb894
haba yosr <76392230+yossHB@users.noreply.github.com>
2021-12-12 17:05:09 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:sampleproject/pages/film_template.dart';
 
-void main() => runApp(MaterialApp(
-      home: Film(),
-    ));
+void main(){
+      return runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        fontFamily: 'PT-Sans',
+      ),
+       home: Film(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
